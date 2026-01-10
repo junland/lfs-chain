c36f2c3053ac67c9918e74280ecbc41dadbcbac6
John Unland <unlandj2012@gmail.com>
2026-01-10 22:48:19 +0000

    Update .gitignore and add sources.list for build dependencies

--- build-stage1.sh
+++ build-stage1.sh
@@ -3,9 +3,14 @@
 set -euo pipefail
 
 TOOLCHAIN_TARBALL_URL="https://toolchains.bootlin.com/downloads/releases/toolchains/x86-64/tarballs/x86-64--glibc--stable-2025.08-1.tar.xz"
-TOOLCHAIN_TARBALL="x86-64--glibc--stable-2025.08-1.tar.xz"
-TOOLCHAIN_DIR=${TOOLCHAIN_DIR:-"/opt/toolchain"}
-TARGET_BUILD_DIR=${TARGET_BUILD_DIR:-"/opt/target-build"}
+TOOLCHAIN_DIR=${TOOLCHAIN_DIR:-"${PWD}/toolchain"}
+TOOLCHAIN_TARBALL_FILENAME=${TOOLCHAIN_TARBALL_FILENAME:-"x86-64-glibc-stable-2025.08-1.tar.xz"}
+TARGET_ROOTFS_DIR=${TARGET_ROOTFS_DIR:-"${PWD}/target-rootfs"}
+TARGET_TRIPLET="${TARGET_TRIPLET:-x86_64-linux-gnu}"
+
+SOURCES_DIR=${SOURCES_DIR:-"${TARGET_ROOTFS_DIR}/sources"}
+SOURCES_LIST=${SOURCES_LIST:-"${PWD}/data/sources.list"}
+SOURCES_BUILD_DIR=${SOURCES_BUILD_DIR:-"${TARGET_ROOTFS_DIR}/build"}
 
 extract_file() {
 	local archive_file=$1
@@ -18,7 +23,7 @@ extract_file() {
 	# Make sure the archive file exists, if not find another archive file with a different extension.
 	if [ ! -f "${archive_file}" ]; then
 		msg "Archive file ${archive_file} does not exist, searching for alternative..."
-		archive_file=$(find "${SOURCES}" -name "$(basename "${archive_file}" | sed 's/\.[^.]*$//').*" | head -1)
+		archive_file=$(find "${SOURCES_DIR}" -name "$(basename "${archive_file}" | sed 's/\.[^.]*$//').*" | head -1)
 		if [ ! -f "${archive_file}" ]; then
 			msg "Error: Archive file ${archive_file} does not exist."
 			exit 1
@@ -68,25 +73,50 @@ extract_file() {
 
 echo "Downloading toolchain tarball..."
 
-mkdir -p "${TOOLCHAIN_DIR}"
-wget -O "${TOOLCHAIN_DIR}/${TOOLCHAIN_TARBALL}" "${TOOLCHAIN_TARBALL_URL}"
+mkdir -vp "${TOOLCHAIN_DIR}"
+
+wget -O "${TOOLCHAIN_DIR}/${TOOLCHAIN_TARBALL_FILENAME}" "${TOOLCHAIN_TARBALL_URL}"
 
 echo "Extracting toolchain..."
 
-extract_file "${TOOLCHAIN_DIR}/${TOOLCHAIN_TARBALL}" "${TOOLCHAIN_DIR}"
+extract_file "${TOOLCHAIN_DIR}/${TOOLCHAIN_TARBALL_FILENAME}" "${TOOLCHAIN_DIR}"
 
 echo "Setting up target build directory..."
 
-mkdir -p "${TARGET_BUILD_DIR}"
+mkdir -p "${TARGET_ROOTFS_DIR}"
 
 # Run toolchain relocate script if it exists
 if [ -f "${TOOLCHAIN_DIR}/relocate-toolchain.sh" ]; then
     echo "Relocating toolchain..."
-    bash "${TOOLCHAIN_DIR}/relocate-toolchain.sh" "${TARGET_BUILD_DIR}"
+    bash "${TOOLCHAIN_DIR}/relocate-toolchain.sh" "${TARGET_ROOTFS_DIR}"
 else
     echo "No relocate-toolchain.sh script found, please ensure the toolchain is correctly set up."
     exit 1
 fi
+
+export PATH="${TOOLCHAIN_DIR}/bin:${PATH}"
+
+# Setup directories
+mkdir -vp "${SOURCES_BUILD_DIR}" "${SOURCES_DIR}" "${TARGET_ROOTFS_DIR}"
+
+# Download all source files listed in sources.list file
+echo "Downloading source files..."
+
+wget -nv --tries=15 --waitretry=15 --input-file="${SOURCES_LIST}" --directory-prefix="${SOURCES_DIR}"
+
+# Start the build for m4
+
+echo "Starting build for m4..."
+
+extract_file "${SOURCES_DIR}/m4-1.4.19.tar.gz" "${SOURCES_BUILD_DIR}/m4-src"
+
+cd "${SOURCES_BUILD_DIR}/m4-src"
+
+./configure --prefix="/usr" --host=$TARGET_TRIPLET --build=$(build-aux/config.guess)
+
+make -j$(nproc)
+
+make DESTDIR="${TARGET_ROOTFS_DIR}" install
 
 
 
